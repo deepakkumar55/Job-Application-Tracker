3f5bf6cc4e791672f4cec0ca2cabafa22e6306f0
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2024-08-07 15:34:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
     const [isFormOpen, setIsFormOpen] = useState(false);
 
     useEffect(() => {
+        console.log("Firebase config: ", {
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.REACT_APP_FIREBASE_APP_ID,
+        });
+
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             setUser(user);
         });
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzXMsA-Q_t5NOjzGZJA2-qacFOSsC2dUs",
-  authDomain: "job-application-tracker-89991.firebaseapp.com",
-  projectId: "job-application-tracker-89991",
-  storageBucket: "job-application-tracker-89991.appspot.com",
-  messagingSenderId: "701497406012",
-  appId: "1:701497406012:web:fe8a01cea9815a50520939",
-  measurementId: "G-LFFTFG17EE"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
